ae5fc574eaa6319144d24a594d14a063d4248c04
Shagnik <shagnikpaul.772@gmail.com>
2023-04-06 00:31:02 +0530

    first commit 😎

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello how are you !!!!!!!!!!!!!!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
