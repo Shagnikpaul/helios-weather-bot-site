7cb27d09c95b858d613ce2c231fa9a81046cdf2d
Shagnik <shagnikpaul.772@gmail.com>
2023-05-01 21:17:43 +0530

    Temporarily disabled database

--- backend/settings.py
+++ backend/settings.py
@@ -76,10 +76,7 @@ if DEBUG:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+   
 }
 
 
