028783745dbf2f21a6c3523cee7534220c740b0c
Shagnik <shagnikpaul.772@gmail.com>
2023-05-01 20:56:48 +0530

    Changes in static files

---
+++ build.sh
@@ -0,0 +1,16 @@
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Install the latest version of pip
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
