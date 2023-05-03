03dc5cdfd981c8dea49ee721247e8cfc81f8cb08
Shagnik <shagnikpaul.772@gmail.com>
2023-05-03 10:03:45 +0530

    🎨 Added tailwind CSS, deleted extra frontend

--- frontends/src/App.jsx
+++ frontends/src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <>
-      <img className='cent' src='https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F041%2F943%2F1aa1blank.jpg' ></img>
-      <h1 className='heading'>Cooking™ something Interesting...</h1>
-      <p className='heading'>Work in progress...</p>
+      <img className='cent border-4' src='https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F041%2F943%2F1aa1blank.jpg' ></img>
+      <h1 className='heading text-4xl mt-5'>Cooking™ something Interesting...</h1>
+      <p className='heading mt-5'>Work in progress...</p>
     </>
   )
 }
