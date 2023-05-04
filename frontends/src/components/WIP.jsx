5cc5eb89238d5aa7183e523322dafcd6cee61fbd
Shagnik <shagnikpaul.772@gmail.com>
2023-05-04 20:29:23 +0530

    ✅ Fixed scaliing issues with image files.

--- frontends/src/components/WIP.jsx
+++ frontends/src/components/WIP.jsx
@@ -2,8 +2,11 @@ export default function WIP() {
   return(
     <>
       <div className="bg-blue-200 dark:bg-blue-950 h-full nav-margin text-center">
-        <img className="h-auto mx-auto max-w-xl rounded-lg shadow-xl dark:shadow-gray-800" src="https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F041%2F943%2F1aa1blank.jpg" alt="image description" />
-
+        
+        <div className="mx-auto p-7">
+          <img className="h-auto mx-auto  rounded-xl shadow-xl dark:shadow-gray-800" src="https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F041%2F943%2F1aa1blank.jpg" alt="image description" />
+        </div>
+        
         <h1 className="text-center mt-10 mb-4 text-4xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Cooking something Interesting...</h1>
         <p className='text-center heading mt-5 dark:text-whitemb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400'>
           I might take a while to complete this website; till then, why don't you try the Discord Weather Bot for whom this website is being made? Click on the top-right button to invite the bot, and click the link below for <span className="text-blue-400">instructions</span> on how to use the bot.
